fe960b47599f79668c35801209744064c7535e3a
abusubi <asimkhan23@gmail.com>
2021-09-28 14:04:43 -0500

    Create caseStudies_trackVisited.js

---
+++ code/JS/caseStudies_trackVisited.js
@@ -0,0 +1,56 @@
+super-embed:
+<script>
+
+//Check if page visited.
+//localStorage.setItem('visited-'+window.location.pathname, 'visitedtrue');
+
+//Check and print percent scrolled.
+//If true, record page with prefix visited- in localstorage.
+
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+}
+
+var docheight = getDocHeight()
+
+var winheight, docheight, trackLength, throttlescroll
+
+function getmeasurements(){
+    winheight = window.innerHeight || (document.documentElement
+       || document.body).clientHeight;
+    docheight = getDocHeight();
+    trackLength = docheight - winheight;
+}
+
+function amountscrolled(){
+    var scrollTop = window.pageYOffset || (document.documentElement
+        || document.body.parentNode || document.body).scrollTop;
+    // gets percentage scrolled (ie: 80 or NaN if tracklength == 0);
+    var pctScrolled = Math.floor(scrollTop/trackLength * 100);
+    console.log(pctScrolled + '% scrolled');
+    if (pctScrolled => 75){
+        console.log('Case study marked as visited-');
+    }
+}
+
+getmeasurements()
+
+window.addEventListener("resize", function(){
+    getmeasurements()
+}, false)
+
+window.addEventListener("scroll", function(){
+    clearTimeout(throttlescroll)
+        //Trigger scrollPercent on scroll-timeout.
+        // throttle code inside scroll to once every 50 milliseconds
+        throttlescroll = setTimeout(function(){
+        amountscrolled()
+        //Print Visited- in localstorage and console.
+        }, 50)
+}, false)
+</script>
